8cfb77be951769d084d97dec3028953c0574fea3
NG CHUN HOU <chunhou574197@gmail.com>
2024-06-17 14:39:14 +0800

    nvim init.lua update

--- init.lua
+++ init.lua
@@ -1,56 +1,66 @@
 -- init.lua windows 7/10 location: /c/Users/knxuser/AppData/Local/nvim/init.lua
-vim.cmd("execute pathogen#infect('$VIM/runtime/bundle/{}')")
-require("gruvbox").setup({
-  undercurl = true,
-  underline = true,
-  bold = true,
-  italic = {
-    strings = false,
-    comments = true,
-    operators = false,
-    folds = true,
-  },
-  strikethrough = true,
-  invert_selection = false,
-  invert_signs = false,
-  invert_tabline = false,
-  invert_intend_guides = false,
-  inverse = true, -- invert background for search, diffs, statuslines and errors
-  contrast = "", -- can be "hard", "soft" or empty string
-  palette_overrides = {},
-  overrides = {},
-  dim_inactive = false,
-  transparent_mode = false,
+-- init.lua unix location: ~/.config/nvim/init.lua
+-- rc order: plugin load -> lua script -> vim script
+local lazypath = os.getenv("VIM") .. "/runtime/lazy.nvim"
+local bundlepath =  os.getenv("VIM") .. "/runtime/bundle/"
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  { dir = bundlepath .. "catppuccin", lazy = true },
+  { dir = bundlepath .. "lightline.vim", lazy = false },
+  { dir = bundlepath .. "plenary.nvim", lazy = true },
+  { dir = bundlepath .. "nui.nvim", lazy = true },
+  { dir = bundlepath .. "neo-tree.nvim", lazy = true },
+  { dir = bundlepath .. "telescope.nvim", lazy = false },
+  { dir = bundlepath .. "toggleterm.nvim", lazy = true }
 })
 
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-vim.opt.termguicolors = true
+require("catppuccin").setup()
 
-require("nvim-tree").setup()
+vim.g.lightline = {colorscheme = "catppuccin"}
+
+require("neo-tree").setup({
+  window = {
+    mappings = {
+      ["h"] = {"toggle_node", nowait = false},
+      ["l"] = "open"
+    }
+  }
+})
+
+vim.api.nvim_set_keymap("n", "<C-h>", ":Neotree<cr>", {silent = true, noremap = true})
+
+if package.config:sub(1,1) == "\\" then
+  vim.o.shell = "C:/Program Files/Git/bin/bash.exe"
+  vim.o.shellcmdflag = "-s"
+end
+
+require("toggleterm").setup({
+  open_mapping = [[<C-\>]],
+  direction = 'float'
+})
+
 vim.cmd([[
 set number
+set autochdir
 set splitbelow
 set splitright
 set wildmode=longest,list,full
 set wildmenu
-set tabstop=4
-set shiftwidth=4
+set tabstop=2
+set shiftwidth=2
+set expandtab
 set title
 set clipboard=unnamed
 
+colorscheme catppuccin-mocha
 set background=dark
-colorscheme gruvbox
-let g:lightline = { 'colorscheme': 'gruvbox' }
 
 nnoremap <leader>ff <cmd>Telescope find_files<cr>
 nnoremap <leader>fg <cmd>Telescope live_grep<cr>
 nnoremap <leader>fb <cmd>Telescope buffers<cr>
 nnoremap <leader>fh <cmd>Telescope help_tags<cr>
+nnoremap <leader>fo <cmd>Telescope oldfiles<cr>
 nnoremap <F11> <C-]>
 nnoremap <F12> <C-T>
-nnoremap <leader>e <cmd>NvimTreeToggle<cr>
-
-set shell=\"C:/Program\ Files/Git/bin/bash.exe"\
-nnoremap <leader>t <cmd>split \| terminal<cr>i
 ]])

24831c201776d30f9e2ee752a33fc44bc72442dc
NG CHUN HOU <chunhou574197@gmail.com>
2024-07-04 15:19:51 +0800

    add treesitter and nvim-dap into init.lua

--- init.lua
+++ init.lua
@@ -12,8 +12,63 @@ require("lazy").setup({
   { dir = bundlepath .. "nui.nvim", lazy = true },
   { dir = bundlepath .. "neo-tree.nvim", lazy = true },
   { dir = bundlepath .. "telescope.nvim", lazy = false },
-  { dir = bundlepath .. "toggleterm.nvim", lazy = true }
+  { dir = bundlepath .. "toggleterm.nvim", lazy = true },
+  { dir = bundlepath .. "nvim-treesitter", lazy = false },
+  { dir = bundlepath .. "nvim-dap", lazy = true },
+  { dir = bundlepath .. "nvim-nio", lazy = true },
+  { dir = bundlepath .. "nvim-dap-ui", lazy = true }
 })
+
+require("dapui").setup()
+local dap, dapui = require("dap"), require("dapui")
+
+dap.adapters["pwa-node"] = {
+  type = "server",
+  host = "localhost",
+  port = "${port}",
+  executable = {
+    command = "node",
+    args = { bundlepath .. "js-debug/src/dapDebugServer.js", "${port}"},
+  }
+}
+
+dap.configurations.javascript = {
+  {
+    type = "pwa-node",
+    request = "launch",
+    name = "Launch file",
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = "${workspaceFolder}",
+    args = function()
+      local args_string = vim.fn.input('Arguments: ')
+      return vim.split(args_string, " ")
+    end,
+  },
+}
+
+dap.listeners.before.attach.dapui_config = function()
+  dapui.open()
+end
+dap.listeners.before.launch.dapui_config = function()
+  dapui.open()
+end
+dap.listeners.before.event_terminated.dapui_config = function()
+  dapui.close()
+end
+dap.listeners.before.event_exited.dapui_config = function()
+  dapui.close()
+end
+
+require("nvim-treesitter.configs").setup({
+  highlight = { enable = true }
+})
+
+-- build parser from source if treesitter failed to download parser packages
+-- require("nvim-treesitter.install").prefer_git = false
+-- use windows default native c compiler. use x64 version of cl if neovim is x64
+-- require 'nvim-treesitter.install'.compilers = { "cl" }
 
 require("catppuccin").setup({
   styles = {
@@ -33,6 +88,10 @@ require("neo-tree").setup({
 })
 
 vim.api.nvim_set_keymap("n", "<C-h>", ":Neotree<cr>", {silent = true, noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>db", ":lua require('dap').toggle_breakpoint()<cr>", {silent = true, noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>dc", ":lua require('dap').continue()<cr>", {silent = true, noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>ds", ":lua require('dap').step_over()<cr>", {silent = true, noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>di", ":lua require('dap').step_into()<cr>", {silent = true, noremap = true})
 
 if package.config:sub(1,1) == "\\" then
   vim.o.shell = "C:/Program Files/Git/bin/bash.exe"
